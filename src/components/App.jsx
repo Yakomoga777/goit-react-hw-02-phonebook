9d9c4a761292b105d0e8b70624d0bd6d5dcc42fc
Roman Petrenko <rpetrenko0105@gmail.com>
2023-04-23 14:38:54 +0300

    додає фільтр

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,73 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { nanoid } from 'nanoid';
 
 import { Contacs } from './Contacts/Contacts';
 import { GlobalStyle } from './GlobalStyle/GlobalStyle';
 import { Phonebook } from './Phonebook/Phonebook';
 
 const theme = {};
+const INITIAL_STATE = {
+  name: '',
+  number: '',
+};
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
+    number: '',
   };
 
   handleSubmit = event => {
     event.preventDefault();
     const form = event.target;
     const name = form.elements.name.value;
-    this.setState(prevState => {
-      return { name: name, contacts: [...[name]] };
-    });
+    // console.log(event.target.elements[name]);
+    const number = form.elements.number.value;
+    this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        {
+          name: name,
+          number: number,
+          id: nanoid(),
+        },
+      ],
+    }));
+
+    this.reset();
+  };
+  reset = () => {
+    this.setState({ ...INITIAL_STATE });
   };
 
-  addNewContact() {
-    console.log(this.state);
+  handleChangeName = evt => {
+    this.setState({ name: evt.target.value });
+  };
+  handleChangeNumber = evt => {
+    this.setState({ number: evt.target.value });
+  };
+
+  handleChangeFilter = event => {
+    this.setState({ filter: event.target.value });
+    this.filtersContacts();
+  };
+
+  filtersContacts() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(item =>
+      item.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+    );
+    this.setState({ contacts: filteredContacts });
+    console.log(filteredContacts);
   }
-  // onChangeInput(event) {
-  //   const contactName = event.target.value;
-  //   console.log(contactName);
-  //   return contactName;
-  //   // this.setState(prevState => {});
-  // }
-
-  //Створюємо метод додавання контакту
-  // addNewConract(event) {
-  //   event.preventDefault();
-
-  //   // const item = this.onChangeInput();
-  //   // console.log(item);
-  //   console.log('add');
-  // }
 
   // Функція рендеру:
   render() {
@@ -54,12 +82,25 @@ export class App extends Component {
             handleSubmit={event => {
               this.handleSubmit(event);
             }}
-            // onInput={event => {
-            //   this.onChangeInput(event);
-            // }}
+            valueName={this.state.name}
+            valueNumber={this.state.number}
+            onNameInput={event => {
+              this.handleChangeName(event);
+            }}
+            onNumberInput={event => {
+              this.handleChangeNumber(event);
+            }}
           />
         </div>
-        <Contacs title="contacts" items={this.state.contacts} />
+        <Contacs
+          title="contacts"
+          items={this.state.contacts}
+          // onFilterInput={this.onFilter()}
+          valuefilter={this.state.filter}
+          onFilterInput={event => {
+            this.handleChangeFilter(event);
+          }}
+        />
       </ThemeProvider>
     );
   }
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 export class Phonebook extends Component {
   render() {
-    const { title, btn, handleSubmit, onInput } = this.props;
+    const {
+      title,
+      btn,
+      handleSubmit,
+      onNameInput,
+      valueName,
+      onNumberInput,
+      valueNumber,
+    } = this.props;
     return (
       <div>
         <h3>{title}</h3>
@@ -13,7 +21,17 @@ export class Phonebook extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            // onChange={onInput}
+            value={valueName}
+            onChange={onNameInput}
+          />
+          <input
+            type="tel"
+            name="number"
+            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={valueNumber}
+            onChange={onNumberInput}
           />
           <button type="submit">{btn}</button>
         </form>
