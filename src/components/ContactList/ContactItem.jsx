20e346f80096ab4df3143b5e179263a0751d0401
Roman Petrenko <rpetrenko0105@gmail.com>
2023-04-25 12:11:21 +0300

    редагує експорт ContactItem.jsx

--- src/components/ContactList/ContactItem.jsx
+++ src/components/ContactList/ContactItem.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StiledItem } from './ContactItem.styled';
 
 export function ContactItem({ id, name, number, handleDelete }) {
